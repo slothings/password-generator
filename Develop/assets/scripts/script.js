17c5bb1efa6471c95b6e00ec09cd435c0b4beeb9
Slothing <seth.r.martineau@gmail.com>
2020-08-08 08:11:16 -0600

    adding my index.html, script.js, style.css

---
+++ Develop/assets/scripts/script.js
@@ -0,0 +1,75 @@
+// Assignment Code
+
+var generateBtn = document.querySelector("#generate");
+
+// Setting up variables, .split lets me break the whole string into smaller separate string
+
+var characterUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var characterUpperArray = characterUpper.split("");
+
+var characterLower = "abcdefghijklmnopqrstuvwxyz";
+var characterLowerArray = characterLower.split("");
+
+var number = "0123456789";
+var numberArray = number.split("");
+
+var specialCharacter = "`~!@#$%^&*()-_=+[{]};:',<.>/?";
+var specialCharacterArray = specialCharacter.split("");
+
+// Function to generate password
+
+function writePassword() {
+
+  // Variables to collect user input
+
+  var allCharacters = [];
+  var resultPassword = "";
+  var TotalPasswordLength = prompt("How many characters would you like your password to be?");
+
+  if (TotalPasswordLength < 8 || TotalPasswordLength > 128) {
+    alert("Your password must be between 8-128 characters!")
+  }
+
+  // Conditions for password, Array.prototype.push.apply lets me add the content of multiple arrays into one array
+
+  else {
+
+    if (confirm("Do you want your password to have uppercase letters?")) {
+      Array.prototype.push.apply(allCharacters, characterUpperArray);
+    }
+
+    if (confirm("Do you want your password to have lowercase letters?")) {
+      Array.prototype.push.apply(allCharacters, characterLowerArray);
+    }
+
+    if (confirm("Do you want your password to have numbers?")) {
+      Array.prototype.push.apply(allCharacters, numberArray);
+    }
+
+    if (confirm("Do you want your password to have special characters?")) {
+      Array.prototype.push.apply(allCharacters, specialCharacterArray);
+    }
+
+    if (allCharacters.length === 0) {
+      alert("You must select at least 1 constraint for your password")
+    }
+
+    //Run loop with selected constraints
+    
+    else {
+
+      for(var i = 0; i < TotalPasswordLength; i++) {
+        var random = Math.floor(Math.random()*allCharacters.length);
+        resultPassword += allCharacters[random];
+      }
+
+    }
+
+  }
+
+  document.getElementById("password").innerHTML = resultPassword;
+}
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
